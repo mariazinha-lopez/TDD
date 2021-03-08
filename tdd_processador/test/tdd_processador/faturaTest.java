aced57f1d52eaa9d6beabc3b9a3541725fe0ed78
Maria López <marialopez@id.uff.br>
2021-03-08 02:40:01 -0300

    Criação do teste "testAdicionaBoletoNaListaBoletos"

--- tdd_processador/test/tdd_processador/faturaTest.java
+++ tdd_processador/test/tdd_processador/faturaTest.java
@@ -11,4 +11,14 @@ public class faturaTest {
 		Fatura f = new Fatura("2020-01-22", 200.50, "Wladimir Tourinho", "NÃO PAGO");
 		Assertions.assertEquals(200.50, f.getValorTotal());
 	}
+	
+	@Test
+	public void testAdicionaBoleto() {
+		Boleto boleto = new Boleto ("23423423", "2020-01-23", 200.50);
+		Boleto boleto2 = new Boleto ("23234467", "2019-05-19", 2220.50);
+		Fatura f = new Fatura("2020-01-22", 200.50, "Wladimir Tourinho", "NÃO PAGO");
+		f.adicionarBoleto(boleto);
+		f.adicionarBoleto(boleto2);
+		Assertions.assertTrue(f.getBoletos().contains(boleto) && f.getBoletos().contains(boleto2));
+	}
 }
